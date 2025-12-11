72d979c895d81c6091afa0f8df598dde7b7a668c
Arthur Castro <57842754+ArthurCastro99@users.noreply.github.com>
2025-12-11 12:51:28 -0300

    tentativa 17

--- src/components/Destaque/index.jsx
+++ src/components/Destaque/index.jsx
@@ -7,14 +7,14 @@ const Destaques = () => {
       <div className="gridPromocoes">
         <div className="cardPromo">
           <img
-            src="./public/menuImagens/carbonara.webp"
+            src="menuImagens/carbonara.webp"
             alt="Spaghetti Carbonara"
           />
           <h3>Spaghetti Carbonara</h3>
           <p>de R$ 45 por R$ 35</p>
         </div>
         <div className="cardPromo">
-          <img src="./public/menuImagens/classica.webp" alt="Lasanha" />
+          <img src="menuImagens/classica.webp" alt="Lasanha" />
           <h3>Lasanha</h3>
           <p>De R$ 50 por R$ 40</p>
         </div>
