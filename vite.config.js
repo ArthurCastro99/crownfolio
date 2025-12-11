286f88cf23ad70a2b3f641947919b68d6fe7ffc2
Arthur Castro <57842754+ArthurCastro99@users.noreply.github.com>
2025-12-11 12:23:41 -0300

    Removi o stats e adicionei um novo build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      filename: "./stats.html",
-      open: true,
-    }),
-  ],
+  plugins: [react()],
+  base: "/Crownfolio/",
 });
